1f5d1d6212d2bd2d713726633fa6b9fd10c7852e
Kristen Pairitz <ktpairit@gmail.com>
2016-02-15 16:59:55 -0500

    separate index file pieces into header & footer files w php code

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Web Interactivity and Engagement</title>
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+<?php wp_head(); ?>
+</head>
+
+<body>
+
+    <div class="container">
+        <header class="row">
+            <div class="twelve columns">
+                <h1>Web Interactivity and Engagement</h1>
+                <p>This is my blank WordPress template.</p>
+            </div>
+        </header>
+        <!-- END OF HEADER - BEGIN SECTION CONTENT -->
--- index.php
+++ index.php
@@ -1,35 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <title>Web Interactivity and Engagement</title>
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
-</head>
-
-<body>
-
-    <div class="container">
-        <header class="row">
-            <div class="twelve columns">
-                <h1>Blank WordPress Template</h1>
-                <p>This is my WordPress template.</p>
-            </div>
-        </header>
-        <!-- END OF HEADER - BEGIN SECTION CONTENT -->
+<?php get_header(); ?>
         <section class="row">
             <div class="twelve columns">
                 <p>Body content goes here.</p>
             </div>
         </section>
-        <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
-        <footer class="row">
-            <div class="twelve columns">
-                <p>Footer content goes here.</p>
-            </div>
-        </footer>
-    </div> <!-- ends container -->
-
-
-</body>
-
-</html>
+<?php get_footer(); ?>
