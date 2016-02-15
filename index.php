d7e6d35c382520342bf347544342c95353fced43
Kristen Pairitz <ktpairit@gmail.com>
2016-02-15 14:49:15 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Web Interactivity and Engagement</title>
+</head>
+
+<body>
+    <h1>Web Interactivity and Engagement</h1>
+    <h2>Spring 2016</h2>
+
+    <p>This is where blogs go.</p>
+</body>
+
+</html>
