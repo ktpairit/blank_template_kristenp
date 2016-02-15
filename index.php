16928512c9aa90317564137f5eaf6fe8ec2cfe68
Kristen Pairitz <ktpairit@gmail.com>
2016-02-15 16:42:49 -0500

    change index.php file content & link to stylesheet

--- index.php
+++ index.php
@@ -3,16 +3,32 @@
 
 <head>
     <title>Web Interactivity and Engagement</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 
 <body>
-    <h1>Web Interactivity and Engagement</h1>
-    <h2>Spring 2016</h2>
 
-    <p>This is where blogs go.</p>
+    <div class="container">
+        <header class="row">
+            <div class="twelve columns">
+                <h1>Blank WordPress Template</h1>
+                <p>This is my WordPress template.</p>
+            </div>
+        </header>
+        <!-- END OF HEADER - BEGIN SECTION CONTENT -->
+        <section class="row">
+            <div class="twelve columns">
+                <p>Body content goes here.</p>
+            </div>
+        </section>
+        <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
+        <footer class="row">
+            <div class="twelve columns">
+                <p>Footer content goes here.</p>
+            </div>
+        </footer>
+    </div> <!-- ends container -->
 
-    <p><?php echo "This will parse on the server, but display on the browser" ?></p>
 
 </body>
 
